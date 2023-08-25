d05b410bc71c5cfa55c335988e6657cf203bb13b
Maxim Lisovskiy <chinatri@gmail.com>
2023-08-25 17:11:31 +0300

    fixed error, using try-except

--- single_number.py
+++ single_number.py
@@ -1,10 +1,13 @@
 from collections import Counter
 class Solution(object):
     def singleNumber(self, nums):
-        new_dict = Counter(nums)
-        for key, value in new_dict.items():
-            if value == 1:
-                return key
+        try:
+            new_dict = Counter(nums)
+            for key, value in new_dict.items():
+                if value == 1:
+                    return key
+        except:
+            return "You need enter the list"
 
 result = Solution()
-print(result.singleNumber([4, 1, 2, 1, 2]))
+print(result.singleNumber(1))
