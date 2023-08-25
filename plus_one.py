05b9ecba544c753c854a7df989dc2078cd68d51c
Maxim Lisovskiy <chinatri@gmail.com>
2023-08-25 17:26:44 +0300

    use try-except and type annotations

--- plus_one.py
+++ plus_one.py
@@ -1,13 +1,17 @@
 
 class Solution(object):
-    def plusOne(self, digits):
-        number = int("".join(map(str, digits)))
-        number += 1
-        new_list = str(number)
-        new_list_1 = []
-        for i in new_list:
-            new_list_1.append(int(i))
-        return new_list_1
+    def plusOne(self, digits: list):
+        try:
+            number = int("".join(map(str, digits)))
+            number += 1
+            new_list = str(number)
+            new_list_1 = []
+            for i in new_list:
+                new_list_1.append(int(i))
+            return new_list_1
+        except:
+            return "You neet to use list"
 
 result = Solution()
 print(result.plusOne([4, 3, 2, 1]))
+
