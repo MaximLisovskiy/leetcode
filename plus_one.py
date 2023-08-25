d11f400a01c8a006aa61a52cf83f621b2c4ac4c6
Maxim Lisovskiy <chinatri@gmail.com>
2023-08-25 18:45:00 +0300

    change

--- plus_one.py
+++ plus_one.py
@@ -1,16 +1,13 @@
 
 class Solution(object):
     def plusOne(self, digits: list) -> list:
-        try:
-            number = int("".join(map(str, digits)))
-            number += 1
-            new_list = str(number)
-            new_list_1 = []
-            for i in new_list:
-                new_list_1.append(int(i))
-            return new_list_1
-        except:
-            return "You neet to use list"
+        number = int("".join(map(str, digits)))
+        number += 1
+        new_list = str(number)
+        new_list_1 = []
+        for i in new_list:
+            new_list_1.append(int(i))
+        return new_list_1
 
 result = Solution()
 print(result.plusOne([4, 3, 2, 1]))
