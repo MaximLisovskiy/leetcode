fe7911aea78bb67ad8f0e4ccc2c907c2e2fd4ac8
Maxim Lisovskiy <chinatri@gmail.com>
2023-08-26 17:24:09 +0300

    change try

--- plus_one.py
+++ plus_one.py
@@ -1,17 +1,19 @@
 
 class Solution(object):
     def plusOne(self, digits: list) -> list:
-        number = int("".join(map(str, digits)))
-        number += 1
         try:
-            convert_to_string = str(number)
-            list_plus_one = []
-            for i in convert_to_string:
-                list_plus_one.append(int(i))
-            return list_plus_one
-        except Exception:
+            number = int("".join(map(str, digits)))
+        except TypeError:
             return "You need the list"
+        number += 1
+        convert_to_string = str(number)
+        list_plus_one = []
+        for i in convert_to_string:
+            list_plus_one.append(int(i))
+        return list_plus_one
+
+
 
 result = Solution()
-print(result.plusOne([2]))
+print(result.plusOne(1))
 
