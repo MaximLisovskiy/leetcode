eb79e19e82ad8916928ab2401f1d8b96e907a3ce
Maxim Lisovskiy <chinatri@gmail.com>
2023-08-26 13:16:05 +0300

    use try

--- plus_one.py
+++ plus_one.py
@@ -3,15 +3,15 @@ class Solution(object):
     def plusOne(self, digits: list) -> list:
         number = int("".join(map(str, digits)))
         number += 1
-        convert_to_string = str(number)
-        list_plus_one = []
         try:
+            convert_to_string = str(number)
+            list_plus_one = []
             for i in convert_to_string:
                 list_plus_one.append(int(i))
             return list_plus_one
-        except:
+        except Exception:
             return "You need the list"
 
 result = Solution()
-print(result.plusOne([4, 3, 2, 1]))
+print(result.plusOne([2]))
 
