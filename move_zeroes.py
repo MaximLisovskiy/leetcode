f450c0e4de361af7fa8f95f2e370167ecac1b12c
Maxim Lisovskiy <chinatri@gmail.com>
2023-09-08 11:25:09 +0300

    Use try-except

--- move_zeroes.py
+++ move_zeroes.py
@@ -1,10 +1,13 @@
 class Solution:
-    def moveZeroes(self, nums):
-        zero_list = [i for i in nums if i == 0]
+    def moveZeroes(self, nums: list) -> list:
+        try:
+            zero_list = [i for i in nums if i == 0]
+        except Exception:
+            return "Enter the list"
         for i in zero_list:
             nums.remove(i)
             nums.append(0)
         return nums
 
 result = Solution()
-print(result.moveZeroes([0, 1, 0, 3, 12]))
+print(result.moveZeroes([0, 1, 3, 0, 12]))
