646d8de6a65ff861b0a4be1cb9dbd8dff74722e2
Maxim Lisovskiy <chinatri@gmail.com>
2023-09-08 11:27:37 +0300

    Use try-except

--- single_number.py
+++ single_number.py
@@ -4,11 +4,11 @@ class Solution(object):
     def singleNumber(self, nums: list) -> int:
         try:
             new_dict = Counter(nums)
-            for key, value in new_dict.items():
-                if value == 1:
-                    return key
         except:
             return "You need enter the list"
+        for key, value in new_dict.items():
+            if value == 1:
+                return key
 
 result = Solution()
 print(result.singleNumber([1, 2, 1, 2, 4]))
