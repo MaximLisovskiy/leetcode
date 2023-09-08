2bebe3cc26814f4d233dffee21a68bc2f098ce5e
Maxim Lisovskiy <chinatri@gmail.com>
2023-09-08 11:33:35 +0300

    Use try-except

--- containsDuplicate.py
+++ containsDuplicate.py
@@ -1,6 +1,10 @@
 class Solution(object):
-    def containsDuplicate(self, nums):
-        return True if len(set(nums)) < len(nums) else False
+    def containsDuplicate(self, nums: list) -> bool:
+        try:
+            return True if len(set(nums)) < len(nums) else False
+        except:
+            return "Enter the list"
+
 
 result = Solution()
-print(result.containsDuplicate([2, 3, 4, 5, 6]))
+print(result.containsDuplicate([1, 2, 3, 4]))
